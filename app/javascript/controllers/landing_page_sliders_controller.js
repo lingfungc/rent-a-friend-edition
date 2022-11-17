80a8cf272d952f95a562113cddf7017e7646a2f2
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-17 23:14:25 +0000

    added javascript autoplay timer for home page slideshow

--- app/javascript/controllers/landing_page_sliders_controller.js
+++ app/javascript/controllers/landing_page_sliders_controller.js
@@ -8,20 +8,33 @@ export default class extends Controller {
     const buttons = document.querySelectorAll("[data-carousel-button]");
 
     buttons.forEach(button => {
-    button.addEventListener("click", () => {
-    console.log("hello, javascript");
+      button.addEventListener("click", () => {
+        console.log("hello, javascript");
 
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-    const slides = button.closest("[data-carousel").querySelector("[data-slides]");
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1;
 
-    const activeSlide = slides.querySelector("[data-active]");
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-    if (newIndex < 0) newIndex = slides.children.length - 1;
-    if (newIndex >= slides.children.length) newIndex = 0;
+        const slides = button.closest("[data-carousel").querySelector("[data-slides]");
+        const activeSlide = slides.querySelector("[data-active]");
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+        if (newIndex < 0) newIndex = slides.children.length - 1;
+        if (newIndex >= slides.children.length) newIndex = 0;
 
-    slides.children[newIndex].dataset.active = true;
-    delete activeSlide.dataset.active
-  })
-})
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active;
+      })
+    })
+
+    setInterval(() => {
+      console.log("hello, javascript setInterval");
+
+      const slides = document.querySelector("[data-slides]");
+      const activeSlide = slides.querySelector("[data-active]");
+      let newIndex = [...slides.children].indexOf(activeSlide) + 1;
+      if (newIndex < 0) newIndex = slides.children.length - 1;
+      if (newIndex >= slides.children.length) newIndex = 0;
+
+      slides.children[newIndex].dataset.active = true;
+      delete activeSlide.dataset.active;
+    }, 10000);
   }
 }
