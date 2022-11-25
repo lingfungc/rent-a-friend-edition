95c1125cc66aa02639fa57af9a9a845796f6189e
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-25 15:55:34 +0000

    add assets link_to media directory to link the video

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module RentAFriend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # config.assets.paths << "#{Rails.root}/app/assets/media"
   end
 end
