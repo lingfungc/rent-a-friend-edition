18f06bd5910df7ab4beffa228402856623be99db
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-27 21:45:00 +0000

    updated booking start and end date validation in model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,14 +2,23 @@ class Booking < ApplicationRecord
   belongs_to :friend
   belongs_to :user
 
-  validates :start_date, presence: true
-  validates :end_date, presence: true, comparison: { greater_than: :start_date }
-  # validates :total_price, presence: true, numericality: { only_float: true }
+  validetes :start_date, :end_date, presence: true, availabilitiy: true
   validate :start_date_cannot_be_in_the_past
+  validate :end_date_after_start_date
+  # validates :start_date, presence: true
+  # validates :end_date, presence: true, comparison: { greater_than: :start_date }
+
+  # validates :total_price, presence: true, numericality: { only_float: true }
 
   private
 
   def start_date_cannot_be_in_the_past
     errors.add(:start_date, "can't be in the past") if start_date.present? && start_date < Date.today
+  end
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    errors.add(:end_date, "must be after the start date") if end_date < start_date
   end
 end
