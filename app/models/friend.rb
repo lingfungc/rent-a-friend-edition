d04757895c32bd1cdc45e0f721895262c2c49c3b
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-27 23:33:54 +0000

    created unavailable_dates method in friend model to check itself bookings

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,8 +1,8 @@
 class Friend < ApplicationRecord
   belongs_to :user
-  has_one_attached :photo # to attach images linked in Cloudinary
-  has_many :bookings, dependent: :destroy # Destroy bookings when friend is deleted
-  geocoded_by :location # Mapping the location attribute of friend
+  has_one_attached :photo                   # Attach images linked in Cloudinary
+  has_many :bookings, dependent: :destroy   # Destroy bookings when friend is deleted
+  geocoded_by :location                     # Mapping the location attribute of friend
   after_validation :geocode, if: :will_save_change_to_location?
 
   validates :first_name, :last_name, :bio, :categories, :pronouns, :location, :age, :daily_rate, presence: true
@@ -11,6 +11,16 @@ class Friend < ApplicationRecord
   validates :categories, inclusion: { in: %w[sports music funeral family date travel] }
   validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 18 }
   validates :daily_rate, numericality: { greater_than: 0 }
+
+  def unavailable_dates
+    # Use .pluck get the values from the hash by the given keys
+    # Then these values which are start_date and end_date is stored in range as an array temporarily
+    # Next use .map to create an array of hashes which have [:from] and [:to] as keys
+    # While range[0] and range[1] which are start_date and end_date as values
+    bookings.pluck(:start_date, :end_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 
   include PgSearch::Model
   pg_search_scope :search_by_categories_and_location_and_age,
