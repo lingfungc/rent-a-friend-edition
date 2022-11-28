1c28132679a6c6df17d8175b95b613a2925e6d77
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-28 15:03:57 +0000

    updated flatpickr controller and booking form in friends#show

--- app/validators/availability_validator.rb
+++ app/validators/availability_validator.rb
@@ -1,7 +1,7 @@
 class AvailabilityValidator < ActiveModel::EachValidator
-  # record = the qureied friend instance
+  # record = the queried friend instance in the booking model
   # attribute = either start_date or end_date which is what we want to check and validate
-  # value = whatever is currently assigned to start_date and end_date
+  # value = whatever is currently assigned to start_date and end_date which is the user's input in the form
   def validate_each(record, attribute, value)
     bookings = Booking.where(['friend_id =?', record.friend_id])
     date_ranges = bookings.map { |booking| booking.start_date..booking.end_date }
@@ -11,3 +11,6 @@ class AvailabilityValidator < ActiveModel::EachValidator
     end
   end
 end
+
+# added the below line in booking.rb
+# validates :start_date, :end_date, presence: true, availability: true
