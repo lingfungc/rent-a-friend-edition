682bf8586072128733b543b70d8ac21a33891536
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-29 20:53:17 +0000

    added filter highlight to all new opts

--- app/javascript/controllers/filter_highlight_controller.js
+++ app/javascript/controllers/filter_highlight_controller.js
@@ -6,18 +6,35 @@ export default class extends Controller {
     console.log("Connected to Filter Highlight");
 
     const queryString = window.location.search.split('=');
+    const sort = queryString[0];
     const query = queryString[1];
-    // console.log(query);
 
-    const filters = document.querySelectorAll("#categories")
+    // in case that query is undefined
+    if (query == null) {
+      let sortKey = document.querySelector(".fa-globe");
+      let sortFilter = sortKey.closest('.filter-btn');
+      console.log(sortFilter);
+      sortFilter.classList.add('hover-active');
+    }
+
+    let sortKey = document.querySelector("#trending");
+    if (sort === '?trending') {
+      let sortFilter = sortKey.closest('.filter-opt').firstElementChild.lastElementChild.firstElementChild;
+      console.log(sortFilter);
+      sortFilter.classList.add('hover-active');
+    }
+
+    sortKey = document.querySelector("#new");
+    if (sort === '?new') {
+      let sortFilter = sortKey.closest('.filter-opt').firstElementChild.lastElementChild.firstElementChild;
+      console.log(sortFilter);
+      sortFilter.classList.add('hover-active');
+    }
+
+    const filters = document.querySelectorAll("#categories");
 
     filters.forEach((filter) => {
-      // console.log(filter);
-      // console.log(filter.value);
-
       if (query === filter.value) {
-        // console.log(`Hi, this is ${filter.value}`);
-
         const queryFilter = filter.closest('.filter-opt').firstElementChild.lastElementChild.firstElementChild;
         console.log(queryFilter);
         queryFilter.classList.add('hover-active');
