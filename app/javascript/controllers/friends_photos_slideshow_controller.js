2dd4b70d7d8546d44ccd098a78f49dd7462e4afc
Ling Fung Chan <justinlfchan@gmail.com>
2022-12-01 02:46:12 +0000

    added prev right btn for friends photos in index page

--- app/javascript/controllers/friends_photos_slideshow_controller.js
+++ app/javascript/controllers/friends_photos_slideshow_controller.js
@@ -3,6 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="friends-photos-slideshow"
 export default class extends Controller {
   connect() {
-    console.log("Connected to friends photos slideshow stimulus");
+    console.log("Connected to Friends Photos Slideshow Stimulus");
   }
 }
