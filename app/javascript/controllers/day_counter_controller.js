fa18ab51211bbb8a5036bcce2369fd631a91319f
Ling Fung Chan <justinlfchan@gmail.com>
2022-12-08 17:52:52 +0000

    added dots query selector for current next prev dot and updated dots function

--- app/javascript/controllers/day_counter_controller.js
+++ app/javascript/controllers/day_counter_controller.js
@@ -11,18 +11,18 @@ export default class extends Controller {
     const booking_end_date = document.getElementById("booking_end_date");
 
     booking_start_date.addEventListener("change", (event) => {
-      console.log(booking_start_date.value)
+      // console.log(booking_start_date.value)
       // console.log(test1);
       const first_date = new Date(booking_start_date.value)
 
       booking_end_date.addEventListener("change", (event) => {
-        console.log(booking_end_date.value)
+        // console.log(booking_end_date.value)
         // console.log(test2);
         const last_date = new Date(booking_end_date.value)
         const total_days = (last_date - first_date)/(1000*60*60*24) + 1
 
         const daily_price = this.positionValue;
-        console.log(daily_price)
+        // console.log(daily_price)
         const total_price = total_days * daily_price
 
         const price = document.querySelector('#total-price');
