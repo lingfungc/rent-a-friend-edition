025fd565365bb1eae375647d9fa5b93d5e36a782
Ling Fung Chan <justinlfchan@gmail.com>
2022-12-11 21:47:51 +0000

    connected to friend-img-swap stimulus

--- app/javascript/controllers/friend_img_swap_controller.js
+++ app/javascript/controllers/friend_img_swap_controller.js
@@ -3,5 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="friend-img-swap"
 export default class extends Controller {
   connect() {
+    console.log("Connected to Friend Img Swap Stimulus");
+  }
+
+  swap() {
+    console.log("Connected to Friend Img Swap Stimulus Swap Method");
   }
 }
