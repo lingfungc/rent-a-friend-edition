05632cedb9d88bdc21fa1d0a227e61f369d78a42
Ling Fung Chan <justinlfchan@gmail.com>
2022-12-11 23:18:41 +0000

    completed basic logic for friend-img-swap method

--- app/javascript/controllers/friend_img_swap_controller.js
+++ app/javascript/controllers/friend_img_swap_controller.js
@@ -6,11 +6,35 @@ export default class extends Controller {
 
   connect() {
     console.log("Connected to Friend Img Swap Stimulus");
+
+    // const currentImg = document.querySelector('.current-img');
+    // console.log(currentImg);
+
+    const track = document.querySelector('.friend-img-right');
+    console.log(track);
+
+    const images = [...track.children];
+    console.log(images);
+
+    images.forEach((image) => {
+      image.addEventListener('click', e => {
+
+        console.log(e.target);
+        console.log(e.target.closest('.photo'));
+        console.log(e.target.parentElement);
+
+        let currentImg = document.querySelector('.current-img');
+        e.target.parentElement.insertAdjacentElement('afterbegin', currentImg);
+        currentImg.classList.remove('current-img');
+
+        const mainImage = document.querySelector('.friend-img-left');
+        mainImage.insertAdjacentElement('afterbegin', e.target);
+        e.target.classList.add('current-img');
+      })
+    });
   }
 
-  swap() {
-    console.log("Connected to Friend Img Swap Stimulus Swap Method");
-    // console.log(this.mainTarget);
-    // console.log(this.subTarget);
-  }
+  // swap() {
+  //   console.log("Connected to Friend Img Swap Stimulus Swap Method");
+  // }
 }
